d5cb993f12790f464f3e2be041cb622a72d99a07
Mendy ABESSIRA <mabessir@e2r9p13.42.fr>
2017-12-06 16:26:39 +0100

    Add some on gnl.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mabessir <mabessir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 11:28:27 by mabessir          #+#    #+#             */
-/*   Updated: 2017/12/06 15:34:20 by mabessir         ###   ########.fr       */
+/*   Updated: 2017/12/06 16:19:37 by mabessir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 int	get_next_line(const int fd, char **line)
 {
 	int			i;
-	static char	*str;
 	int			len;
-	int			glen;
+	static char	*str;
+	char		save;
 
 	i = 0;
 	if ((fd < 0 || (!str && !(str = ft_strnew(BUFF_SIZE)))))
 		return (-1);
-	while (i = read(fd, str, BUFF_SIZE) > 0)
+	while (ft_strchr(str, '\n') == NULL && i = read(fd, str, BUFF_SIZE) > 0)
 	{
 		len = ft_strlen(str) - ft_strlen(ft_strchr(str, '\n'));
 		*line = ft_strsub(str, 0, len);
-		str =;
+		save = ft_strchr(str, '\n');
 		i++;
 	}
 	return (i == 0 ? 0 : -1);
